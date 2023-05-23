1878793e4e080aabb428964a2dde21a0c17b4007
CsMuj <mujtaba.chowdhury@csedge.org>
2023-05-23 12:10:59 -0700

    GamePush1

---
+++ game_core.py
@@ -0,0 +1,43 @@
+import time
+import random
+import # Look into multiprocessing
+
+# The code is bit scuffed together this is itteration 1
+
+class entity:
+    # Constuctor the blueprints for creating a class
+    def __init__(self, name, level = 100, health = random.randint(5,10), attack = random.randint(1,3), defence = random.randint(1,3), skill = ["Punch"]):
+        self.name = name
+        # Levels are calculated by dividing the value by a 100. 1 level per 100 exp
+        self.level = level
+        self.health = health
+        self.attack = attack
+        self.defence = defence
+        self.skill = skill
+    def __str__(self):
+        # If you try to print the object the following string is returned
+        return f"\n{self.name}: Level {int(self.level / 100)}, Health {self.health}, Attack {self.attack}, Defence {self.defence}"
+
+    def levelUp(self, exp):
+        if self.level / 100 != self.level + exp:
+            time.sleep(1)
+            print("level Up! ", self.level / 100 + 1)
+            self.health += random.randint(1,2)
+            self.attack += random.randint(1,2)
+            self.defence += random.randint(1,2)
+
+        self.level += exp
+    
+    def damage(self, defender):
+        print("Attack")
+        defender.health =- self.attack - defender.defence + 1
+        print(f"{defender.name} is at {defender.health} health!")
+
+def fight(entity1, entity2):
+    # Requires more testing, runs player and enemy attacks at the same time.
+    if __name__ == '__main__':
+        Thread(target = entity1.damage, args=(entity2).start()
+        Thread(target = entity2.damage, args=(entity1).start()
+    
+mob1 = entity("Itachi", 10, 2, 2, ["Punch", "FireBall"])
+
